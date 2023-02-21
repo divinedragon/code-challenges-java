c4461ebed5bf8924bfbcd037de0cce185c1dae25
Deepak Shakya <justdpk@gmail.com>
2023-02-21 23:55:49 +0530

    GH-57: Solution for CodingBat-Warmup2 - AltPairs

---
+++ codingbat/src/main/java/io/divinedragon/java/codingbat/warmup2/AltPairs.java
@@ -0,0 +1,42 @@
+package io.divinedragon.java.codingbat.warmup2;
+
+import static io.divinedragon.java.common.NumberConstants.TWO;
+import static io.divinedragon.java.common.StringConstants.EMPTY;
+
+import java.util.Optional;
+
+public final class AltPairs {
+
+    public String altPairs(final String input) {
+
+        final String str = Optional.ofNullable(input).orElseGet(() -> EMPTY);
+
+        final StringBuilder result = new StringBuilder();
+
+        if (str.length() <= TWO) {
+            result.append(str);
+        } else {
+            int count = 0;
+            int currentCharPos = TWO * count;
+            int nextCharPos = currentCharPos + 1;
+            final int stringLength = str.length();
+
+            do {
+
+                result.append(str.charAt(currentCharPos));
+
+                if (nextCharPos < stringLength) {
+                    result.append(str.charAt(nextCharPos));
+                }
+
+                count += TWO;
+
+                currentCharPos = TWO * count;
+                nextCharPos = currentCharPos + 1;
+
+            } while (currentCharPos < stringLength);
+        }
+
+        return result.toString();
+    }
+}
