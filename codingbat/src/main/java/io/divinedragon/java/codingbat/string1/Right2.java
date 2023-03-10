5757dcbb574a3ee9eeb7a579c918469c3bc1fb5b
Deepak Shakya <justdpk@gmail.com>
2023-03-10 21:50:00 +0530

    GH-104: Solution for CodingBat - String1 - Right2

---
+++ codingbat/src/main/java/io/divinedragon/java/codingbat/string1/Right2.java
@@ -0,0 +1,22 @@
+package io.divinedragon.java.codingbat.string1;
+
+import static io.divinedragon.java.common.NumberConstants.TWO;
+import static io.divinedragon.java.common.StringConstants.EMPTY_STRING_SUPPLIER;
+
+import java.util.Optional;
+
+public class Right2 {
+
+    public String right2(final String input) {
+
+        final String value = Optional.ofNullable(input).orElseGet(EMPTY_STRING_SUPPLIER);
+
+        final int valueLength = value.length();
+
+        if (valueLength <= TWO) {
+            return value;
+        }
+
+        return value.substring(valueLength - TWO) + value.substring(0, valueLength - TWO);
+    }
+}
