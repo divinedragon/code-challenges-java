9288adaaf94f3ce49fdcc5e7db558cd2f0cae2dd
Deepak Shakya <justdpk@gmail.com>
2023-08-06 01:49:53 +0530

    GH-7: Fix PMD Warning for System.out.println

--- hackerrank/src/main/java/io/divinedragon/java/hackerrank/intro/WelcomeToJava.java
+++ hackerrank/src/main/java/io/divinedragon/java/hackerrank/intro/WelcomeToJava.java
@@ -3,7 +3,7 @@ package io.divinedragon.java.hackerrank.intro;
 public class WelcomeToJava {
 
     public void print() {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        System.out.println("Hello, World."); // NOPMD
+        System.out.println("Hello, Java."); // NOPMD
     }
 }
